0dd5ff4095bbde9e38976b7ce511935e3ee903c5
Severin Ibarluzea <seveibar@gmail.com>
2015-12-05 04:36:38 -0500

    auto-removes users with illegal characters, users sorted by name fixes #11

--- pdfgen.js
+++ pdfgen.js
@@ -7,13 +7,19 @@ var doc = new pdf();
 
 doc.pipe(fs.createWriteStream('output.pdf'));
 
+users.sort(function(a,b){
+    return a.name.localeCompare(b.name);
+});
+
 for (var i = 0;i < users.length;i++){
     var user = users[i];
     doc.addPage()
         .fontSize(24)
         .text(user.name, 100,100)
+        .fontSize(14)
+        .text(user.rcsid, 100, 120)
         .image("./output/" + util.normalizeName(user.name) + "/profile.jpg",50,150,{
-            fit: [540, 1000]
+            fit: [540, 400]
         });
 }
 
--- users.js
+++ users.js
@@ -1,11 +1,18 @@
 var fs = require("fs");
 
 // Read all user data
-var users_raw = fs.readFileSync('users.json', 'utf8').split("\n");
+var users_raw = fs.readFileSync('collections/users.json', 'utf8').split("\n");
 var users = [];
 for (var i = 0;i < users_raw.length;i++){
     if (users_raw[i]){
         users.push(JSON.parse(users_raw[i]));
+    }
+}
+
+// Remove any invalid users
+for (var i = users.length-1;i>=0;i--){
+    if (/^[a-zA-Z0-9- ]*$/.test(users[i].name) == false){
+        users.splice(i,1);
     }
 }
 
