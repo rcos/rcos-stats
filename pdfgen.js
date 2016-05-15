8e84b5d65a3dbc4cc33f4b7a3c49c0d64085c211
Kiana McNellis <kmcnellis@users.noreply.github.com>
2016-05-15 14:44:44 -0400

    remove ghost code

--- pdfgen.js
+++ pdfgen.js
@@ -17,33 +17,6 @@ users.sort(function(a,b){
     return la.localeCompare(lb);
 });
 
-//
-// for (var i = 0;i < users.length;i++){
-//     var user = users[i];
-//
-//     doc.addPage()
-//         .fontSize(24)
-//         .text(user.name, 25,25)
-//         .fontSize(9)
-//         .text("Github: "+user.github.login, 25,55)
-//         .text("Email: "+user.email, 25,65)
-//         .text("Attendence (large group): "+user.attendance.length, 25,75)
-//         .text("Projects: "+((function(user){
-//             if (!user || ! "projects" in user || !user.projects || user.projects == undefined){
-//                 return "None";
-//             }
-//             return user.projects.map(function (value){
-//                 return projects.projectMap.get(value.$oid);
-//             }).reduce(function(previousValue, currentValue){
-//                 return previousValue + currentValue.name + " ("+currentValue.repositories + "),  ";
-//             }, "");
-//
-//         })(user)), 25,85)
-//
-//         .image("./output/" + user.dirName + "/profile.jpg",25,250,{
-//             fit: [540, 1000]
-//         });
-// }
 for (var i = 0;i < users.length;i++){
     var user = users[i];
     var info = allInfo[user._id.$oid];
